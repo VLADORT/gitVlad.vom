e7bae832879d5f2eaf830cf7816037a0b0a9b4d1
VLADORT <vlad300820001@gmail.com>
2018-12-04 22:40:49 +0200

    Ladt commit

--- src/TestApp.java
+++ src/TestApp.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import package.SimpleCalc;
 public static void main(String[]args){
 float a=10;
 float b=(float) 12.5;
-System.out.println("a+b="+(a+b));
+System.out.println("a+b="+getSUM(a,b));
         System.out.println("GIT test application");
+
         };
