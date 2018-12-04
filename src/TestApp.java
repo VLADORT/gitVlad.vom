5bc7451ffa4f368067307646331b2dfd901bb739
VLADORT <vlad300820001@gmail.com>
2018-12-04 23:10:29 +0200

    ijsdnjsndjsdjsdn

--- src/TestApp.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-import package.SimpleCalc;
-public static void main(String[]args){
-float a=10;
-float b=(float) 12.5;
-System.out.println("a+b="+getSUM(a,b));
-        System.out.println("GIT test application");
-
-        };
---
+++ src/main.java
@@ -0,0 +1,15 @@
+package testApp;
+
+import java.util.Scanner;
+import package.SimpleCalc;
+public class main {
+
+    public static void main(String[]args){
+        float a=10;
+        float b=(float) 12.5;
+        System.out.println("a+b="+getSUM(a,b));
+        System.out.println("GIT test application");
+
+    };
+
+}
