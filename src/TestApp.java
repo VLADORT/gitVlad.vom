f7bdd64398c7313846dc583a083f439fc9f6d8a5
VLADORT <vlad300820001@gmail.com>
2018-12-04 21:33:08 +0200

    Third commit

--- src/TestApp.java
+++ src/TestApp.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public static void main(String[]args){
-float a=15;
-float b=(float) 12.0;
+float a=10;
+float b=(float) 12.5;
 System.out.println("a+b="+(a+b));
         System.out.println("GIT test application");
         };
