92aa89772f204fcf298d07123ec5c07ea440be69
VLADORT <vlad300820001@gmail.com>
2018-12-04 20:14:17 +0200

    Second commit

--- src/TestApp.java
+++ src/TestApp.java
@@ -1,5 +1,7 @@
-public class TestApp {
-    public static void main(String[]args){
+import java.util.Scanner;
+public static void main(String[]args){
+float a=15;
+float b=(float) 12.0;
+System.out.println("a+b="+(a+b));
         System.out.println("GIT test application");
-    };
-};
+        };
